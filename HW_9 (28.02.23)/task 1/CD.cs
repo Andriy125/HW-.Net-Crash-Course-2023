3ecfdd510fcc1c07f437f775b0f3a4edbe94f183
Andriy125 <57405296+Andriy125@users.noreply.github.com>
2023-03-21 00:22:25 +0200

    Add files via upload

---
+++ HW_9 (28.02.23)/task 1/CD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW_9__28._02._23_
+{
+    internal class CD : Disk, IRomoveableDisk
+    {
+        bool hasDisk;
+        public bool HasDisk { get; } = false;
+        public string Name { get; set; }
+        public CD (string? name, string? memory, int memSize) : base(memory, memSize)
+        {
+            Name = name;
+        }
+
+        public new string GetName()
+        {
+            return Name;
+        }
+        public void Insert()
+        {
+            hasDisk = true;
+            Console.WriteLine($"CD: {Name} Inserted");
+        }
+        public void Reject()
+        {
+            hasDisk = false;
+            Console.WriteLine($"CD: {Name} Rejected");
+        }
+    }
+}
---
+++ HW_9 (28.02.23)/task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using HW_9__28._02._23_;
+
+HDD hdd = new HDD()
+{
+    Memory = "0110101",
+    MemSize = 250
+};
+DVD dvd = new DVD()
+{
+    Memory = "00000000000011111",
+    MemSize = 5
+};
+Printer printer = new Printer();
+Monitore monitor = new Monitore();
+
+List<Disk> disks = new List<Disk>
+{
+    hdd, dvd
+};
+
+List<IPrintInformation> printDevice = new List<IPrintInformation>
+{
+    printer, monitor
+};
+
+Comp computer = new Comp(disks, printDevice);
+computer.PrintInfo("123", printDevice[0]);
