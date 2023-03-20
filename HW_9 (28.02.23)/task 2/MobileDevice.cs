825616ee4af8409103e05d17f4304c16059147ec
Andriy125 <57405296+Andriy125@users.noreply.github.com>
2023-03-21 00:21:23 +0200

    Add files via upload

---
+++ HW_9 (28.02.23)/task 2/MobileDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_9__28._02._23__task_2
+{
+    internal class MobileDevice
+    {
+    }
+}
